63fd1c2425e75026a4214a01083a5e5cfc68b0b2
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2019-09-25 18:45:53 +0200

    removed busyness logic from MetaController

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,14 +1,16 @@
 <?php
+/**
+ * Routes for irods owncloud plugin
+ *
+ * Author: Stefan Wolfsheimer stefan.wolfsheimer@surfsara.nl
+ * License: Apache License 2.0
+ *
+ */
 namespace OCA\files_irods\AppInfo;
 $application = new Application();
 
 $application->registerRoutes($this, [
     'routes' => [
-        [
-            'name' => 'VirtualCollections#get',
-            'url' => '/api/virtual',
-            'verb' => 'GET'
-        ],
         [
             'name' => 'Meta#get',
             'url' => '/api/meta/{path}',
