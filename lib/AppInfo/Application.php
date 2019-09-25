9280309150f0daed5a3bdaa5d25db40362396e79
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2019-09-25 18:51:04 +0200

    refactored Application.php

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,11 +1,18 @@
 <?php
-
+/**
+ * Implementation of OwnCloud StorageAdapter
+ * see \OCP\Files\Storage
+ * see https://github.com/owncloud/core/blob/v10.0.10/lib/public/Files/Storage/StorageAdapter.php
+ * see https://doc.owncloud.org/server/10.2/developer_manual/app/advanced/custom-storage-backend.html
+ *
+ * Author: Stefan Wolfsheimer stefan.wolfsheimer@surfsara.nl
+ * License: Apache License 2.0
+ *
+ */
 namespace OCA\files_irods\AppInfo;
 
 use \OCP\AppFramework\App;
-
 use OCP\Files\External\Config\IBackendProvider;
-use OCA\files_irods\Controller\VirtualCollectionsController;
 use OCA\files_irods\Controller\MetaController;
 
 
@@ -16,17 +23,14 @@ class Application extends App implements IBackendProvider {
         $backendService = $container->getServer()->getStoragesBackendService();
 		$backendService->registerBackendProvider($this);
 
-        $container->registerService('VirtualCollectionsController', function($c) {
-            // register the controller in the container
-            return new VirtualCollectionsController(
-                $c->query('AppName'),
-                $c->query('Request'));
-        });
         $container->registerService('MetaController', function($c) {
             // register the controller in the container
             return new MetaController(
                 $c->query('AppName'),
                 $c->query('Request'));
+        });
+        $container->registerService('Config', function($c) {
+            return $c->query("ServerContainer")->getConfig();
         });
     }
 
