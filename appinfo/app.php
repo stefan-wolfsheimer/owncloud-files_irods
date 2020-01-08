edc841126a6999fc42e8f0e073cac84376d24d3f
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2020-01-08 16:12:27 +0100

    removed navigation menu entry

--- appinfo/app.php
+++ appinfo/app.php
@@ -9,17 +9,3 @@
 namespace OCA\files_irods\AppInfo;
 
 $app = new Application();
-
-\OC::$server->getNavigationManager()->add(function ()
-{
-    $urlGenerator = \OC::$server->getURLGenerator();
-    return
-        [
-            // The string under which your app will be referenced in owncloud
-            'id' => 'files_irods',
-
-            // The sorting weight for the navigation.
-            // The higher the number, the higher will it be listed in the navigation
-            'order' => 10,
-        ];
-});
